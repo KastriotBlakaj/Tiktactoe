f261a10c374e5000a65af9869cbcfd8a13d8f7c1
kastriot <lotiblakaj123@gmail.com>
2021-10-10 21:23:01 +0200

    FinalTouch

--- src/Board.kt
+++ src/Board.kt
@@ -1,7 +1,9 @@
+import kotlin.math.pow
+
 class Board(private val n :Int){
     private val empty="___"
     private  var moveCount=0
-    private var isGameOver=false
+    var isGameOver=false
     var board = Array(n){Array(n){empty} }
 
     fun reserBoard(){
@@ -11,10 +13,95 @@ class Board(private val n :Int){
     fun printBoard(){
         board.forEach{row ->
             row.forEach{element ->
-                print("|$element|")
+                if(element==empty){
+                    print("|$element|")
+                }else {
+                    print("| $element |")
+                }
+
             }
             println()
         }
         println()
+    }
+
+    fun placePiece(x: Int, y: Int,move:String){
+        if(!isGameOver
+            && isPositionValid(x,y)
+            && board[x][y]==empty
+            ){
+            moveCount++
+            board[x][y]=move
+            printBoard()
+            isGameOver=isWiningMonve(x,y,move)||isDraw()
+            if(isGameOver && isDraw()){
+                println("We have a winner")
+            }
+            else if(isDraw()){
+                println("Draw")
+            }
+        }
+    }
+
+    private fun isPositionValid(x:Int,y:Int):Boolean{
+        return ((x in 0 until n)&& (y in 0 until n))
+    }
+
+    private fun isWiningMonve(x:Int,y:Int,move:String):Boolean{
+        //check the row
+        for(i in 0 until n){
+            if(board[x][i]!=move){
+                break
+            }
+            if(i==n-1){
+                return true //winner here
+            }
+        }
+            //check for the cloumn
+
+        for(i in 0 until n){
+            if(board[i][y]!=move){
+                break
+            }
+            if(i==n-1){
+                return true //winner here
+            }
+        }
+          //Check for diagonal
+        if(x==y) {
+            for (i in 0 until n) {
+                if (board[i][i] != move)
+                    break
+
+                if (i == n - 1) {
+                    return true
+                }
+            }
+        }
+        //Check for diagonal e dyte
+
+
+        if(x+y==n-1)
+        {
+            for(i in 0 until n){
+                if(board[i][(n-1)-i]!=move){
+                    break
+                }
+                if( i==n-1){
+                    true
+                }
+            }
+        }
+        return false
+    }
+
+    fun isDraw():Boolean{
+        return (moveCount==(n.toDouble().pow(2)-1).toInt())
+    }
+    fun resetGame(){
+        reserBoard()
+        isGameOver=false
+        moveCount=0
+
     }
 }
--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,50 @@
-private var board=Board(0)
+import java.util.*
+import javax.print.attribute.IntegerSyntax
 
+private var board=Board(0)
+private val player1="x"
+private val player2="o"
+private var currectPlayer=""
+private var answer=""
 fun main(){
-    board=Board(3 )
+    println("Add size of the board")
+    val scanner = Scanner(System.`in`)
+    val size=Integer.parseInt(scanner.nextLine())
+    board=Board(size )
+        //board.placePiece(0,0,"X")
     board.printBoard()
+
+    while(!board.isGameOver){
+        takeTurns()
+        println("$currectPlayer's turn")
+        println("Enter row number: ")
+        var row =Integer.parseInt(scanner.nextLine())
+        println("Enter column number: ")
+        var column =Integer.parseInt(scanner.nextLine())
+        board.placePiece(row-1,column-1,currectPlayer)
+
+        if(board.isGameOver){
+            println("Want another game? type y")
+            var answer=scanner.nextLine()
+            if(isPlayingAgain(answer))
+                board.resetGame()
+        }
+
+    }
 }
+
+fun takeTurns(){
+    currectPlayer=if(player1==currectPlayer){
+        player2
+    }else
+    {
+        player1
+    }
+
+
+}
+
+fun  isPlayingAgain(answer:String):Boolean{
+    return (answer.equals("y",ignoreCase = true))
+}
+
